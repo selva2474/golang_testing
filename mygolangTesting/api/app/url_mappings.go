bbe5a2b3cb7bbbda6b38fc4b84e243d9434b7c11
selva2474 <selvamurugan881@gmail.com>
2022-06-04 08:06:41 +0530

    chore: just another one

--- mygolangTesting/api/app/url_mappings.go
+++ mygolangTesting/api/app/url_mappings.go
@@ -2,8 +2,6 @@ package app
 
 import "github.com/selva2474/golang_testing/src/api/controllers"
 
-// import "github.com/golang-testing/mygolangTesting/api/controllers"
-
 func mapURLs() {
 	router.GET("/locations/countries/:country_id", controllers.GetCountry)
 }
